ddf08bfbcebfe4ff5c10358915db117023cbfefb
Abhishek <nit.abhi85@gmail.com>
2014-12-23 10:46:16 +0530

    new addition

--- csvcreators/sngapparelinc.php
+++ csvcreators/sngapparelinc.php
@@ -45,7 +45,8 @@
     	$csvData[$count]['name'] = $tempData['title'];
     	$csvData[$count]['price'] = $tempData['price'];
     	$csvData[$count]['qty'] = 1; /*TODO : Check what QTY will be*/
-
+    	$csvData[$count]['weight'] = 0;
+    	
     	$csvData[$count]['short_description'] = $tempData['title'].' - '.$tempData['price'];
     	$csvData[$count]['description'] = 'WL '.$value['category'].' '.$tempData['title'].' at $'.$tempData['price'];
 
@@ -63,12 +64,16 @@
     	$csvData[$count]['image_label'] = '';
     	$csvData[$count]['small_image_label'] = '';
     	$csvData[$count]['thumbnail_label'] = '';
-    	$csvData[$count++]['media_gallery'] = '';
+    	$csvData[$count]['media_gallery'] = '';
+    	
+    	$csvData[$count]['made'] = '';
+    	$csvData[$count]['fabric'] = '';
+    	$csvData[$count][''] = '';
     }
 
     // Writing Header
-    fputcsv($fp, array_keys($csvData[0]));
-
+//    fputcsv($fp, array_keys($csvData[0]));
+exit;
     foreach ($csvData as $data) {
 	    fputcsv($fp, $data);
 	}
@@ -77,7 +82,97 @@
 
     
 
+function flatenData($data) {
+      $tempData = json_decode($data['data'],TRUE);
 
+    	// print_r($tempData);exit;
+
+    	$csvData[$count]['store'] = 'default';
+    	$csvData[$count]['sku'] = 'WL'.$value['category'].'#'.$value['id'];
+    	// $csvData[$count]['in_stock'] = ;
+
+    	$csvData[$count]['name'] = $tempData['title'];
+    	$csvData[$count]['price'] = $tempData['price'];
+    	$csvData[$count]['qty'] = 1; /*TODO : Check what QTY will be*/
+    	$csvData[$count]['weight'] = 0;
+    	
+    	$csvData[$count]['short_description'] = $tempData['title'].' - '.$tempData['price'];
+    	$csvData[$count]['description'] = 'WL '.$value['category'].' '.$tempData['title'].' at $'.$tempData['price'];
+
+    	$csvData[$count]['meta_title'] = $tempData['title'];
+    	$csvData[$count]['meta_description'] = $tempData['title'].' - '.$tempData['price'];
+
+    	$csvData[$count]['attribute_set'] = 'Default';
+    	$csvData[$count]['pack_size'] = $tempData['pack'];
+
+    	$csvData[$count]['categories'] = $value['category'].'/'.$value['subcategory'];
+
+    	$csvData[$count]['image'] = $tempData['images'][0]['large'];
+    	$csvData[$count]['small_image'] = $tempData['images'][0]['small'];
+    	$csvData[$count]['thumbnail'] = $tempData['images'][0]['small'];
+    	$csvData[$count]['image_label'] = '';
+    	$csvData[$count]['small_image_label'] = '';
+    	$csvData[$count]['thumbnail_label'] = '';
+    	$csvData[$count]['media_gallery'] = '';
+    	
+    	$csvData[$count]['made'] = '';
+    	$csvData[$count]['fabric'] = '';
+}
 
 ?>
 
+
+{"images":[
+  {"small":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/thumbnail\/320x480\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8059blk_2.jpg',"
+  ,"large":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/image\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8059blk_2.jpg'"},
+  {"small":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/thumbnail\/320x480\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ss8059_1_2.jpg',"
+  ,"large":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/image\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ss8059_1_2.jpg'"}
+  ],
+
+"title":"Wholesale Womens Plus Size Faux Leather Leggings with Studs",
+"item_no":"SSP8059",
+"size":"ONE SIZE (STRETCH XL TO 4XL)",
+"pack":"6PCS\/PREPACK",
+"fabric":"90% POLYESTER\r\n10% SPANDEX",
+"made":"CHINA",
+"price":"5.50",
+"color_size":[{"color":"Black","size":{"onesize":"6"}}]}
+
+
+{"images":[{
+    "small":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/thumbnail\/320x480\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8056_black_d_1.jpg',",
+    "large":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/image\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8056_black_d_1.jpg'"},
+    {"small":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/thumbnail\/320x480\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8056_black_d_0.jpg',",
+    "large":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/image\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8056_black_d_0.jpg'"},
+    {"small":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/thumbnail\/320x480\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8056_black_l_0.jpg',",
+    "large":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/image\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8056_black_l_0.jpg'"},
+    {"small":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/thumbnail\/320x480\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8056_black_d_2.jpg',",
+    "large":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/image\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8056_black_d_2.jpg'"},
+    {"small":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/thumbnail\/320x480\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8056_red_l_1.jpg',",
+    "large":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/image\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8056_red_l_1.jpg'"},
+    {"small":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/thumbnail\/320x480\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8056_grey_l_2.jpg',",
+    "large":" 'http:\/\/sngapparelinc.com\/media\/catalog\/product\/cache\/1\/image\/9df78eab33525d08d6e5fb8d27136e95\/s\/s\/ssp8056_grey_l_2.jpg'"}],
+    
+"title":"Wholesale Womens Plus Size Faux Leather Leggings with Zippers",
+"item_no":"SSP8056",
+"size":" 1X\/2X 3X\/4X",
+"pack":"6PCS\/PREPACK",
+"fabric":"92% POLYESTER\r\n8% SPANDEX",
+"made":"CHINA",
+"price":"5.50",
+"color_size":[
+        {"color":"Burgundy","size":{"xlxxl":"3","xxxlxxxxl":"3"}},
+        {"color":"Black","size":{"xlxxl":"3","xxxlxxxxl":"3"}},
+        {"color":"Charcoal","size":{"xlxxl":"3","xxxlxxxxl":"3"}}]}
+
+
+
+
+
+
+
+
+
+
+
+  
