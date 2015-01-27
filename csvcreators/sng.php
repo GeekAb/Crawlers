a2aa27d10ad24e9b0a4062e31e67ba2908be9219
Abhishek <nit.abhi85@gmail.com>
2015-01-27 09:53:43 +0530

    iChanges

--- csvcreators/sng.php
+++ csvcreators/sng.php
@@ -1,22 +1,12 @@
 <?php 
 
-	// Include config and initiate
-// include_once __DIR__ . '/../scripts/config/default_config.php';
-// includeMyFiles();
+// Include config and initiate
 include_once __DIR__ . '/../scripts/config/default_config.php';
 include_once __DIR__ . '/../scripts/config/database.php';
 include_once __DIR__ . '/../scripts/config/log.php';
 include_once __DIR__ . '/common.php';
 
-$categoryArray = array( 'Basic'             => array('Printed','Jeggings','Sublimation','Leather','Fitness'),
-                        'Pants'             => array('Palazzo','Jeans','Harem','Shorts'),
-                        'Plus Size'         => array(
-                                                    'Plus Size Leggings'    => array('Basic Plus Size','Printed Plus Size','Jeggings Plus Size','Sublimation Plus Size','Leather Plus Size','Fitness Plus Size'),
-                                                    'Plus Size Pants'    => array('Palazzo Plus Size','Jeans Plus Size','Harem Plus Size','Shorts Plus Size')
-                                                )
-                    );
-
-	// Get Database
+// Get Database
 $db = new Db();
 
 $data = $db->query("SELECT * from products_data WHERE source='sngapparelinc'");
@@ -26,86 +16,126 @@ $csvData = array();
 $count = 0;
 
 $colorSet = array();
-$fp = fopen("output.csv", "a+");
+$sizeArray = array();
+$packArray = array();
 
+$fp = fopen("output.csv", "a+");
 
 foreach ($data as $value) {
 
+    $imgStr = '';
+    $sizeArray = array();
+    $packArray = array();
+    
     $tempData = json_decode($value['data'],TRUE);
 
+    /*Get Category String*/
     $categoryStr = getCategory($value['category'],$value['subcategory']);
-
+    /*Get dimension*/
     $dimension = getDimensions($value['category'],$value['subcategory']);
+    /*Get Sizes*/
+    foreach ($tempData['color_size'] as $cdata) {
+        $keys = array_keys($cdata['size']);
 
-
+        foreach ($keys as $k) {
+            
+            if($k == 'sm') {
+                array_push($sizeArray, 's/m');
+                $packArray['s/m'] = $cdata['size'][$k];
+            }
+            else if($k == 'ml') {
+                array_push($sizeArray, 'm/l');
+                $packArray['m/l'] = $cdata['size'][$k];
+            }
+            else if($k == 'lxl') {
+                array_push($sizeArray, 'l/xl');
+                $packArray['l/xl'] = $cdata['size'][$k];
+            }
+            else if($k == 'xlxxl') {
+                array_push($sizeArray, 'xl/xxl');
+                $packArray['xl/xxl'] = $cdata['size'][$k];
+            }
+            else if($k == 'xxlxxxl') {
+                array_push($sizeArray, 'xxl/xxxl');
+                $packArray['xxl/xxxl'] = $cdata['size'][$k];
+            }
+            else if($k == 'xxxlxxxxl') {
+                array_push($sizeArray, 'xxxl/xxxxl');
+                $packArray['xxxl/xxxxl'] = $cdata['size'][$k];
+            } else {
+                array_push($sizeArray, $k);
+                $packArray[$k] = $cdata['size'][$k];
+            }
+        }
+    } 
+    
     // If not a valid category
     if(strlen($categoryStr) == 0)
         continue;
 
-    $imgStr = '';
     foreach ($tempData['images'] as $img) {
         $imgMain = str_replace(array(",", '\'', '"' ), "", $img['large']);
-        $imgStr .= 'sngapparelinc_'.basename($imgMain).";";
-
+        $imgStr .= $value['id'].'_wl_'.basename($imgMain).";";
         //download_remote_file_with_curl(trim($imgMain));
 
         if(isset($img['small'])) {
             $tempimg = str_replace(array(",", '\'', '"' ), "", $img['small']);
-            //download_remote_file_with_curl(trim($tempimg));
         }
-            
     }
-
 
     foreach ($tempData['color_size'] as $color) {
 
-        // $colorSet[] = $color['color'];
+        foreach ($sizeArray as $size) {
 
-        $csvData[$count]['sku']                       = 'WL'.$value['category'].'#'.$value['id'].'#'.$color['color'];
-        $csvData[$count]['type']                      = 'simple';
-        $csvData[$count]['store']                     = 'default';
+            $csvData[$count]['sku']                       = 'WL'.$value['category'].'#'.$value['id'].'#'.$color['color'];
+            $csvData[$count]['type']                      = 'simple';
+            $csvData[$count]['store']                     = 'default';
 
-        $csvData[$count]['name']                      = $tempData['title'].' '.$color['color'];
-        $csvData[$count]['description']               = $tempData['title'];
-        $csvData[$count]['short_description']         = $tempData['title'];
-        $csvData[$count]['price']                     = $tempData['price'];
-        $csvData[$count]['qty']                       = 1;
-        $csvData[$count]['weight']                    = getWeight($value['category'],$value['subcategory']);
+            $csvData[$count]['name']                      = $tempData['title'].' '.$color['color'];
+            $csvData[$count]['description']               = $tempData['title'];
+            $csvData[$count]['short_description']         = $tempData['title'];
+            $csvData[$count]['price']                     = $tempData['price'];
+            $csvData[$count]['qty']                       = 1;
+            $csvData[$count]['weight']                    = getWeight($value['category'],$value['subcategory']);
 
-        $csvData[$count]['is_in_stock']               = 1;
-        $csvData[$count]['manage_stock']              = 1;
-        $csvData[$count]['use_config_manage_stock']   = 1;
+            $csvData[$count]['is_in_stock']               = 1;
+            $csvData[$count]['manage_stock']              = 1;
+            $csvData[$count]['use_config_manage_stock']   = 1;
 
-        $csvData[$count]['status']                    = 1;
-        $csvData[$count]['visibility']                = '"Catalog, Search"';
-        // $csvData[$count]['categories']                = $value['category'].'/'.$value['subcategory'];
-        $csvData[$count]['categories']                = getCategory($value['category'],$value['subcategory']);
+            $csvData[$count]['status']                    = 1;
+            $csvData[$count]['visibility']                = '"Catalog, Search"';
+            // $csvData[$count]['categories']                = $value['category'].'/'.$value['subcategory'];
+            $csvData[$count]['categories']                = getCategory($value['category'],$value['subcategory']);
 
-        $csvData[$count]['pack']                      = $tempData['pack'];
-        $csvData[$count]['fabric']                    = $tempData['fabric'];
-        $csvData[$count]['made']                      = $tempData['made'];
+            $csvData[$count]['pack']                      = $tempData['pack'];
+            $csvData[$count]['fabric']                    = $tempData['fabric'];
+            $csvData[$count]['made']                      = $tempData['made'];
 
-        $csvData[$count]['source']                    = 'sngapparelinc';
-        $csvData[$count]['source_sku']                = $tempData['item_no'];
+            $csvData[$count]['source']                    = 'sngapparelinc';
+            $csvData[$count]['source_sku']                = $tempData['item_no'];
 
-        $csvData[$count]['tax_class_id']              = 'None';
+            $csvData[$count]['tax_class_id']              = 'None';
 
-        $csvData[$count]['image']                     = 'sngapparelinc_'.basename(str_replace(array(",", '\'', '"' ), "", $tempData['images'][0]['large']));
-        $csvData[$count]['small_image']               = 'sngapparelinc_'.basename(str_replace(array(",", '\'', '"' ), "", $tempData['images'][0]['small']));
-        $csvData[$count]['thumbnail']                 = 'sngapparelinc_'.basename(str_replace(array(",", '\'', '"' ), "", $tempData['images'][0]['small']));
-        $csvData[$count]['image_label']               = $tempData['title'];
-        $csvData[$count]['small_image_label']         = $tempData['title'];
-        $csvData[$count]['thumbnail_label']           = $tempData['title'];
+            $csvData[$count]['image']                     = $value['id'].'_wl_'.basename(str_replace(array(",", '\'', '"' ), "", $tempData['images'][0]['large']));
+            $csvData[$count]['small_image']               = $value['id'].'_wl_'.basename(str_replace(array(",", '\'', '"' ), "", $tempData['images'][0]['small']));
+            $csvData[$count]['thumbnail']                 = $value['id'].'_wl_'.basename(str_replace(array(",", '\'', '"' ), "", $tempData['images'][0]['small']));
+            $csvData[$count]['image_label']               = $tempData['title'];
+            $csvData[$count]['small_image_label']         = $tempData['title'];
+            $csvData[$count]['thumbnail_label']           = $tempData['title'];
 
-        $csvData[$count]['media_gallery']             = $imgStr;
+            $csvData[$count]['media_gallery']             = $imgStr;
 
-        $csvData[$count]['auctioninc_product_length'] = $dimension['length'];
-        $csvData[$count]['auctioninc_product_width']  = $dimension['width'];
-        $csvData[$count]['auctioninc_product_height'] = $dimension['height'];
+            $csvData[$count]['auctioninc_product_length'] = $dimension['length'];
+            $csvData[$count]['auctioninc_product_width']  = $dimension['width'];
+            $csvData[$count]['auctioninc_product_height'] = $dimension['height'];
 
-        $csvData[$count]['attribute_set']             = 'Default';
-        $csvData[$count]['configurable_attributes']   = 'leggingscolor';
-        $csvData[$count++]['leggingscolor']            = $color['color'];
+            $csvData[$count]['attribute_set']             = 'Default';
+            $csvData[$count]['configurable_attributes']   = 'leggingscolor';
+
+            /*Setting size*/
+            $csvData[$count]['size']                       = $size;
+            $csvData[$count++]['leggingscolor']            = $color['color'];
+
     }
 
     $csvData[$count]['sku']                       = 'WL'.$value['category'].'#'.$value['id'];
@@ -137,9 +167,9 @@ foreach ($data as $value) {
     $csvData[$count]['tax_class_id']              = 'None';
 
     
-    $csvData[$count]['image']                     = 'sngapparelinc_'.basename(str_replace(array(",", '\'', '"' ), "", $tempData['images'][0]['large']));
-    $csvData[$count]['small_image']               = 'sngapparelinc_'.basename(str_replace(array(",", '\'', '"' ), "", $tempData['images'][0]['small']));
-    $csvData[$count]['thumbnail']                 = 'sngapparelinc_'.basename(str_replace(array(",", '\'', '"' ), "", $tempData['images'][0]['small']));
+    $csvData[$count]['image']                     = $value['id'].$value['id'].'_wl__wl_'.basename(str_replace(array(",", '\'', '"' ), "", $tempData['images'][0]['large']));
+    $csvData[$count]['small_image']               = $value['id'].$value['id'].'_wl__wl_'.basename(str_replace(array(",", '\'', '"' ), "", $tempData['images'][0]['small']));
+    $csvData[$count]['thumbnail']                 = $value['id'].$value['id'].'_wl__wl_'.basename(str_replace(array(",", '\'', '"' ), "", $tempData['images'][0]['small']));
     $csvData[$count]['image_label']               = $tempData['title'];
     $csvData[$count]['small_image_label']         = $tempData['title'];
     $csvData[$count]['thumbnail_label']           = $tempData['title'];
